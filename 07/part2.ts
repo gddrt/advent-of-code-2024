09796773483b6803759781bf40c457d3b261bf06
Ryan Geddert <ryan@gddrt.com>
2024-12-07 23:25:23 +0900

    day 7 part 2

---
+++ 07/part2.ts
@@ -0,0 +1,31 @@
+import { readLines } from "../utils/fs.ts";
+
+const input = await readLines("input.txt");
+
+type Equation = [number, number[]]
+
+const equations: Equation[] = input.map(x => x.split(': ')).map(x => [Number(x[0]), x[1].split(' ').map(Number)])
+
+const recursivelyOperate = (targetNum: number, firstNum: number, remainingNums: number[]) => {
+    if (remainingNums.length === 1) return (
+        firstNum * remainingNums[0] === targetNum
+        || firstNum + remainingNums[0] === targetNum
+        || Number(firstNum.toString() + remainingNums[0].toString()) === targetNum
+    )
+
+    const newRemainingNums = remainingNums.slice(1);
+    return (
+        recursivelyOperate(targetNum, firstNum + remainingNums[0], newRemainingNums)
+        || recursivelyOperate(targetNum, firstNum * remainingNums[0], newRemainingNums)
+        || recursivelyOperate(targetNum, Number(firstNum.toString() + remainingNums[0].toString()), newRemainingNums)
+    )
+}
+
+const validateEquation = ([result, nums]: Equation): boolean => {
+    const newNums = nums.slice(1)
+    return recursivelyOperate(result, nums[0], newNums);
+}
+
+const partOneAnswer = equations.filter(validateEquation).reduce((a, x) => a + x[0], 0)
+
+console.log(partOneAnswer);
